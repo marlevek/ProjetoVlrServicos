16cbb7c90933bd77addee641fae84d914833db74
marlevek <marcelo_levek@hotmail.com>
2020-06-01 22:17:42 -0300

    fórmulas
    colocadas as fórmulas para calcular o serviço

--- dados.php
+++ dados.php
@@ -1,16 +1,14 @@
 <?php
-session_start();
-$despFixas  = $_GET['despfixas'];
-$despVariav = $_GET['despVariav'];
-$horasServ  = '';
-$capacProd  = 220;
 
-$cvunit     = 275;
-
-
-$impostos   = '';
-$lucro      = '';
-
-
-$total = $_GET['despfixas'] + $_GET['despVariav'];
-echo $total;
+$despFixas  = $_GET['despfixas']; //usuário preencherá
+$despVariav = $_GET['custoVariav']; //usuário preencherá
+$horasServ  = ''; //usuário preencherá
+$capacProd  = 220; //usuário preencherá
+$cfunit     = '$despFixas / $capacProd'; // preenchido automaticamente 
+$cfunitServ = '$cfunit * $horasServ'; //preenchido automaticamente
+$cvunit     = ''; //usuário preencherá
+$cut        = '$cfunit + $cvunit'; //preencido automaticamente
+$impostos   = ''; //usuário preencherá
+$lucro      = ''; //usuário preencherá
+$tm         = '(100/100-($lucro + $impostos))'; //preenchido automaticamente
+$pv         = '$cut * $tm';//esse vai ser preenchido automaticamente e é o valor final do serviço e deve preencher o campo valor do serviço
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-
-<html lang="pt-BRS">
+<html lang="pt-BR">
 
 <head>
   <meta charset="UTF-8">
@@ -12,42 +11,95 @@
 
 <body>
   <div class="container">
-    <h1 style="color:blueviolet">CALCULE O VALOR DO SERVIÇO PRESTADO</h1>
-    <div class="row">
-      <div class="col-sm-12 col-md-6">
-        <form name="vlrServico" action="dados.php" method="GET">
-          <label for="despfixas">Despesas Fixas</label>
-          <input type="text" name="despfixas" style="border-radius: 10%;">
-          <label for="despVariav">Despesas Variáveis</label>
-          <input type="text" name="despVariav" style="border-radius: 10%;">
-          <label for="totaldespesas">TOTAL DESPESAS</label>
-          <input type="text" name="total" style="border-radius: 10%;" value="<?php echo $total; ?>">
-          <input type="submit" value="Calcular">
-          <input type="reset" value="Limpar">
-        </form>
+    <h1 class="text-center my-4" style="color:blueviolet">CALCULE O VALOR DO SERVIÇO PRESTADO</h1>
+    <form name="vlrServico" action="dados.php" method="GET">
+      <div class="form-group row">
+        <label for="despfixas" class="col-sm-2 col-form-label col-form-label-sm">Despesas Fixas</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-sm" id="despfixas" placeholder="R$ 99.999,99" required novalidate>
+        </div>
       </div>
-    </div>
-  </div>
-  <script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-      'use strict';
-      window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-          }, false);
-        });
-      }, false);
-    })();
-  </script>
+      <div class="form-group row">
+        <label for="capProd" class="col-sm-2 col-form-label">Capacidade Produtiva</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="capProd" placeholder="tempo produtivo, em horas, por mês" required novalidate>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="custoFixoUnit" class="col-sm-2 col-form-label">Custo Fixo Unitário</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="custoFixoUnit" readonly>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="horasServ" class="col-sm-2 col-form-label">Tempo do Serviço</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="HorasServ" placeholder="colocar em horas">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="cfunitServ" class="col-sm-2 col-form-label">Custo Fixo unitário do Serviço</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="cfunitServ" readonly>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="impostos" class="col-sm-2 col-form-label">Impostos</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="impostos" placeholder="valor numérico dos impostos sem a porcentagem" required novalidate>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="lucro" class="col-sm-2 col-form-label">Lucro</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="lucro" placeholder="valor do lucro desejado. Por ex. 10%, coloque 10, sem a porcentagem" required novalidate>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="custopVariav" class="col-sm-2 col-form-label">Custo Variável Unitário</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="custoVariav" required novalidate>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="cut" class="col-sm-2 col-form-label">Custo Unitário Total</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="cut" readonly>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="pvServico" class="col-sm-2 col-form-label">Valor do Serviço</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control form-control-lg" id="pvServico" readonly>
+        </div>
+      </div>
+
+
+
+
+
+    </form>
+    <script>
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
+    </script>
 </body>
 
 </html>
