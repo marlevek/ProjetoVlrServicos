be7c1ef587254f514c00282b9aeee77802017eb7
marlevek <marcelo_levek@hotmail.com>
2020-06-25 21:21:18 -0300

    atualizado

--- index.php
+++ index.php
@@ -1,108 +1,119 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+
+<html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Saiba aqui como calcular corretamente o valor do serviço prestado.">
-  <title>Valor do Serviço </title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="calcule aqui o valor do seu serviço de forma correta, e não tenha prejuízo.">
+  <title>Valor do Serviço</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <style>
+    h1 {
+      color: #006633;
+      text-shadow: 2px 2px 2px #006633;
+    }
+
+    h2 {
+      font-size: x-large;
+      color: #000;
+      background-color: mediumaquamarine;
+      text-align: center;
+    }
+
+    hr {
+      color: mediumaquamarine;
+      border: dashed 0.05em;
+    }
+
+    #total {
+      font-size: medium;
+      font-weight: bold;
+      color: darkorange;
+    }
+  </style>
+
 </head>
 
 <body>
+  <img class="img-fluid" src="assets\imagens\img-header-valor-servico.png" alt="imagem cabeçalho valor serviço">
   <div class="container">
-    <h1 class="text-center my-4" style="color:blueviolet">CALCULE O VALOR DO SERVIÇO PRESTADO</h1>
-    <form name="vlrServico" action="dados.php" method="GET">
-      <div class="form-group row">
-        <label for="despfixas" class="col-sm-2 col-form-label col-form-label-sm">Despesas Fixas</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-sm" id="despfixas" placeholder="R$ 99.999,99" required novalidate>
-        </div>
+    <h1 class="text-center mt-4">VALOR DO SERVIÇO PRESTADO</h1>
+    <p>Calcular o valor da prestação do serviço corretamente é importante, pois serve para avaliar se a estrutura de custos da empresa ou do profissional permite a ela ou a ele ser competitivo no mercado.</p>
+    <p>Para calcular o preço de venda na prestação de serviços deve-se conhecer os custos e despesas e também deve ser analisado o preço praticado pela concorrência, pois, os clientes estão pesquisando preços e procurando qualidade, tanto dos serviços quanto do atendimento.</p>
+    <p>Assim, os preços calculados através de fórmulas servirão apenas como um referencial para comparação com os de mercado.</p>
+    <p>Por último, o preço de venda precisa sempre ser revisto, uma vez que pode haver aumento no preço de compra de materiais empregados na prestação dos serviços, e também por exigência dos consumidores ou da concorrência.</p>
+  </div>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-sm-12 col-md-5">
+        <h2>Conceitos Importantes</h2>
+        <h3 style="font-size:larger">Despesas Fixas:</h3>
+        <p>São as despesas que vão incorrer, tem que ser pagas, chova ou faça sol, enquanto a empresa existir.</p>
+        <hr>
+        <h3 style="font-size:larger">CVunit:</h3>
+        <p>Custo variável unitário é o que varia de acordo com a prestação do serviço. Exs.: mão de obra, almoço, materiais diretos, custos de deslocamento, etc</p>
+        <hr>
+        <h3 style="font-size:larger">Custos de deslocamento:</h3>
+        <p>São os custos como: combustível, manutenção do veículo, IPVA, seguro do veículo. Soma tudo e multiplica pelo km gasto para realizar o serviço.</p>
+        <hr>
+        <h3 style="font-size:larger">Impostos + Lucro:</h3>
+        <p>Impostos são os que ocorrem na prestação do serviço e Lucro é o valor que você ou sua empresa deseja ter.</p>
+        <hr>
       </div>
-      <div class="form-group row">
-        <label for="capProd" class="col-sm-2 col-form-label">Capacidade Produtiva</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="capProd" placeholder="tempo produtivo, em horas, por mês" required novalidate>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="custoFixoUnit" class="col-sm-2 col-form-label">Custo Fixo Unitário</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="custoFixoUnit" readonly>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="horasServ" class="col-sm-2 col-form-label">Tempo do Serviço</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="HorasServ" placeholder="colocar em horas">
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="cfunitServ" class="col-sm-2 col-form-label">Custo Fixo unitário do Serviço</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="cfunitServ" readonly>
-        </div>
-      </div>
+      <div class="col-sm-12 col-md-7">
+        <div class="card bg-light">
+          <img src="assets\imagens\img-head-card.jpg" class="card-img-top" alt="imagem cabeçalho card">
+          <form action="#total" method="POST">
+            <!--com action na #total o resultado vai ser mostrado nela, sem subir a pg-->
+            <div class="form-group row mt-4">
+              <label for="despFixa" class="col-sm-3 col-form-label">Desp. Fixas:</label>
+              <div class="col-4">
+                <input type="text" class="form-control" name="despFixa" id="despFixa" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="capacProd" class="col-sm-3 col-form-label">Capac. Produ:</label>
+              <div class="col-4">
+                <input type="text" class="form-control" name="capacProd" id="capacProd" required>
+                <small id="capacProd" class="form-text text-muted">Se não for inteiro use ponto para separar. Ex. 1.5</small>
+              </div>
+            </div>
+            <?php
 
-      <div class="form-group row">
-        <label for="impostos" class="col-sm-2 col-form-label">Impostos</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="impostos" placeholder="valor numérico dos impostos sem a porcentagem" required novalidate>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="lucro" class="col-sm-2 col-form-label">Lucro</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="lucro" placeholder="valor do lucro desejado. Por ex. 10%, coloque 10, sem a porcentagem" required novalidate>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="markup" class="col-sm-2 col-form-label">Markup</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="markup" readonly>
-        </div>
-      </div>
+            $total = "";
 
+            if (isset($_POST['submit'])) {
 
-      <div class="form-group row">
-        <label for="custopVariav" class="col-sm-2 col-form-label">Custo Variável Unitário</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="custoVariav" required novalidate>
+              $despFixa = $_POST['despFixa'];
+              $despFixa = str_replace(',', '.', $despFixa);
+              $capacProd = $_POST['capacProd'];
+              $capacProd = str_replace(',', '.', $capacProd);
+              $total = $despFixa / $capacProd;
+              $total = round($total, 1); //arredondamento valor da divisão
+            }
+
+            ?>
+            <button type="submit" name="submit" class="btn btn-outline-primary">Calcular</button>
+            <div class="form-group row">
+              <label for="total" class="col-sm-3 col-form-label">Total:</label>
+              <div class="col-4">
+                <input type="text" class="form-control" name="total" id="total" value="<?php echo $total; ?>" readonly>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-      <div class="form-group row">
-        <label for="cut" class="col-sm-2 col-form-label">Custo Unitário Total</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="cut" readonly>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label for="pvServico" class="col-sm-2 col-form-label">Valor do Serviço</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control form-control-lg" id="pvServico" readonly>
-        </div>
-      </div>
-    </form>
-    <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-          // Fetch all the forms we want to apply custom Bootstrap validation styles to
-          var forms = document.getElementsByClassName('needs-validation');
-          // Loop over them and prevent submission
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
+    </div>
+  </div>
+  <br><br>
+  <footer>
+    <div class="container">
+      <h5 class="text-center">Desenvolvido por <a href="https://www.mltopsites.com.br" target="new"> ML TOP SITES</a></h5>
+    </div>
+  </footer>
 </body>
 
 </html>
